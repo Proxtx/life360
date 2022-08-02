cc047c9dc2fe8912402656b3f77ede8a82402b45
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-08-02 15:51:45 +0200

    work on data handlers

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import { listen } from "@proxtx/framework";
 import config from "@proxtx/config";
 import { setConfig } from "@proxtx/framework/static.js";
+import { handlers } from "./private/file/collect.js";
+
+console.log(await handlers.users());
 
 setConfig({ ignoreParseHtml: ["/lib/components"] });
 await listen(config.port);
--- private/file/collect.js
+++ private/file/collect.js
@@ -15,7 +15,49 @@ export const data = async (type, start, end, filter = (d) => d) => {
     start = end - start;
   }
   let result = (await getDataInTimespan(folder, start, end)).result;
-  result.filter(filter);
+  for (let i in result) {
+    result[i] = filter(result[i], i);
+  }
+
+  return result;
 };
 
-//export const
+export const handlers = {
+  users: async () => {
+    let users = await data("data", 1, null, (value) => {
+      let res = [];
+      for (let uId in value) {
+        res.push({
+          avatar: value[uId].avatar,
+          name: value[uId].firstName,
+          uId,
+        });
+      }
+
+      return res;
+    });
+    return users[Object.keys(users)[0]];
+  },
+
+  userIds: async () => {
+    let res = await handlers.users();
+    let uIds = [];
+    res.forEach((element) => {
+      uIds.push(element.uId);
+    });
+
+    return uIds;
+  },
+
+  locationsInTimespan: async (users, start, end) => {
+    return await data("locations", start, end, (value) => {
+      for (let k in value) {
+        if (!users.includes(k)) delete value[k];
+      }
+    });
+  },
+
+  overviewMap: async () => {},
+
+  week: 1.6534e-9,
+};
