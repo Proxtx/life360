b684b3c55fe9a7fa417f9740bbc2f8130aea83c0
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-11-17 15:23:01 +0100

    added observe subpage

---
+++ static/observe/main.js
@@ -0,0 +1,27 @@
+import { locations } from "../lib/api.js";
+import { renderLocationData } from "../lib/map.js";
+
+const startButton = document.getElementById("startButton");
+
+startButton.addEventListener("click", async () => {
+  document.body.requestFullscreen();
+  startButton.style.display = "none";
+
+  document.getElementById("map").style.display = "unset";
+
+  showDataLoop();
+});
+
+const showData = async () => {
+  let overview = await locations.getOverview(cookie.pwd);
+  renderLocationData(overview);
+};
+
+const showDataLoop = async () => {
+  try {
+    await showData();
+  } catch {}
+
+  await new Promise((r) => setTimeout(r, 30000));
+  showDataLoop();
+};
