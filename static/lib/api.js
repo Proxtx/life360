ab5c3155e625662482b1acc5be63404698a3c624
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-03-31 16:21:26 +0200

    this is a test ; cookie security

--- static/lib/api.js
+++ static/lib/api.js
@@ -2,3 +2,6 @@ export const meta = await framework.load("meta.js");
 export const data = await framework.load("data.js");
 export const locations = await framework.load("locations.js");
 export const stats = await framework.load("stats.js");
+
+cookie.default =
+  "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; SameSite=Strict;";
