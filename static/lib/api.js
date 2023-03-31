25982a1a8a5fc7ec7895722a8fa893d846c9a1ed
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-03-31 16:34:49 +0200

    this may be working

--- static/lib/api.js
+++ static/lib/api.js
@@ -4,4 +4,4 @@ export const locations = await framework.load("locations.js");
 export const stats = await framework.load("stats.js");
 
 cookie.default =
-  "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; SameSite=Strict;";
+  "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; SameSite=Strict; Secure;";
